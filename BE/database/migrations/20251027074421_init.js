f53e4a86fcc3647ef69fcf81df4f3e0b66d6a6ce
Gary Odendaal <gary@garyo.co.za>
2025-11-10 12:33:08 +0200

    chore: update database migration and seed

--- BE/database/migrations/20251027074421_init.js
+++ BE/database/migrations/20251027074421_init.js
@@ -3,19 +3,29 @@
  * @returns { Promise<void> }
  */
 export async function up(knex) {
-  return knex.schema.createTable('users', function(table) {
-    table.increments('id').primary();
-    table.string('email').notNullable().unique();
-    table.string('password').notNullable();
-    table.timestamps(true, true);
-  }).createTable('posts', function(table) {
-    table.increments('id').primary();
-    table.string('title').notNullable();
-    table.text('content').notNullable();
-    table.integer('user_id').references('id').inTable('users').onDelete('CASCADE');
-    table.timestamps(true, true);
-  });
-};
+  return knex.schema
+    .createTable('users', function (table) {
+      table.increments('id').primary();
+      table.string('email').notNullable().unique();
+      table.boolean('is_archived').notNullable().defaultTo(false);
+      table.string('first_name').nullable();
+      table.string('last_name').nullable();
+      table.string('password').notNullable();
+      table.boolean('verified').notNullable().defaultTo(false);
+      table.timestamps(true, true);
+    })
+    .createTable('posts', function (table) {
+      table.increments('id').primary();
+      table.string('title').notNullable();
+      table.text('content').notNullable();
+      table
+        .integer('user_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE');
+      table.timestamps(true, true);
+    });
+}
 
 /**
  * @param { import("knex").Knex } knex
@@ -23,4 +33,4 @@ export async function up(knex) {
  */
 export async function down(knex) {
   return knex.schema.dropTable('posts').dropTable('users');
-};
+}
--- BE/database/seeds/init.js
+++ BE/database/seeds/init.js
@@ -4,18 +4,20 @@ import bcrypt from 'bcryptjs';
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function seed (knex) {
+export async function seed(knex) {
   await knex('users').del();
   await knex('posts').del();
 
   const hashedPassword = await bcrypt.hash('Admin1234@', 10);
-  const user = await knex('users').insert([
-    {
-      email: 'admin@atlas.co.za',
-      password: hashedPassword,
-    },
-  ]).returning('id');
-
+  const user = await knex('users')
+    .insert([
+      {
+        email: 'admin@atlas.co.za',
+        password: hashedPassword,
+        verified: true,
+      },
+    ])
+    .returning('id');
 
   const post = await knex('posts').insert([
     {
@@ -25,4 +27,4 @@ export async function seed (knex) {
     },
   ]);
   return { user, post };
-};
+}
